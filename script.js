115d81de92bcfa8fa7f81386ab0b70011a9a4214
joaogabriell126 <joaogabriell.126@gmail.com>
2025-11-15 23:52:45 -0300

    :pencil: docs: Adição de comentários entorno do todo o projeto

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const headerMenu = document.getElementById("headerMenu")
 const sideMenu = document.getElementById("sideMenu")
 
+// 1. Lógica para o menu hamburguer
 headerMenu.addEventListener('click', () => {
+    // Remove ou adiciona a classe 'active' para aparecer ou sumir com o menu
     sideMenu.classList.toggle('active');
 
     const isActive = sideMenu.classList.contains('active');
-
+    // Verifica se o menu está ativo ou não trocando os ícones
     if (isActive) {
         headerMenu.classList.remove('fa-bars');
         headerMenu.classList.add('fa-xmark');
@@ -17,9 +19,11 @@ headerMenu.addEventListener('click', () => {
 
 const botoesCategoria = document.querySelectorAll('.btn-category');
 const containerProdutos = document.getElementById('gallery__placeholder');
-
+// Váriavel que vai armazenar todos os produtos 
 let todosOsProdutos = []
+// Esse if, resolve um erro causado pelo script compartilhado em ambas as páginas, permitindo que não haja erros e mantendo o funcionamento padrão dos botões
 if (containerProdutos) {
+    // Cria um Listener para cada botão, pegando o valor da categoria e chamando a função de acordo com o seu valor
     botoesCategoria.forEach(botao => {
         botao.addEventListener('click', () => {
 
@@ -29,8 +33,8 @@ if (containerProdutos) {
         });
     });
 }
-
 function renderizarProdutos(categoriaSelecionada) {
+    // Crie um array com todos os itens filtrados e, em seguida, são jogados um a um no HTML através do .map()
     const itensFiltrados = todosOsProdutos.filter(todosOsProdutos => todosOsProdutos.category == categoriaSelecionada)
 
     const cardsProdutos = itensFiltrados.map(item => {
@@ -43,6 +47,7 @@ function renderizarProdutos(categoriaSelecionada) {
                 </div>
             </div>
             <div class="card-produto__description">
+                <span>id: ${item.id}</span>
                 <h3>${item.title}</h3>
                 <p>${item.description}</p>
             </div>
@@ -55,20 +60,24 @@ function renderizarProdutos(categoriaSelecionada) {
 const galeryRandom = document.getElementById("gallery-random")
 const randomButton = document.getElementById("randomButton")
 
+// A mesma funçãode proteger o Listener
 if (randomButton) {
+    // Chama a função para sortear o produto
     randomButton.addEventListener('click', () => {
         produtoAleatorio();
     })
 }
 
 function produtoAleatorio() {
+    // Guardo em uma váriavel uma número gerado aleatoriamente, incluindo o número mínimo e máximo 
     min = 1;
     max = 30;
     let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-    console.log(randomNumber)
+    // console.log(randomNumber)
 
+    // Procura o primeiro item que tenha o id igual ao número gerado (semelhante ao filter)
     const randomItemFilter = todosOsProdutos.find(produtos => produtos.id == randomNumber)
-    console.log(randomItemFilter)
+    // console.log(randomItemFilter)
 
     const cardHTML = `
         <div class="card-produto">
@@ -79,20 +88,23 @@ function produtoAleatorio() {
                 </div>
             </div>
             <div class="card-produto__description">
+                <span>id: ${randomItemFilter.id}</span>
                 <h3>${randomItemFilter.title}</h3>
                 <p>${randomItemFilter.description}</p>
             </div>
         </div>`
-
+    // gera o card no HTML
     galeryRandom.innerHTML = cardHTML;
 }
 
+// Busca da API com tratamento de erro
 fetch('https://dummyjson.com/products')
     .then(response => {
         if (!response.ok) throw new Error('Erro na resposta da API');
         return response.json();
     })
     .then(data => {
+        // Acessa a parte de produtos e guarda na váriavel lá em cima
         todosOsProdutos = data.products;
     })
     .catch(error => {
